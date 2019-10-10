4e29044ed18e2e0c89615a503dc4ce2dd9998ab5
hsimao <e087754958@gmail.com>
2019-10-10 19:58:19 +0800

    edit: 修改 jest 配置, 改成測試檔案可以隨意放置, 針對所有 .test.js 檔案 運行測試

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0 // functino abc() abc跟() 間隔為0
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
+      files: ['**/*.test.(js|jsx|ts|tsx)'],
       env: {
         jest: true
       }
