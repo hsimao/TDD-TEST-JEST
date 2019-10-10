17534e08be230efe79aaddcd076952e8a853866c
hsimao <e087754958@gmail.com>
2019-10-10 17:28:29 +0800

    add: env

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'vue'
-  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  // 遇到特定副檔名檔案，需要轉換成 jest 可以識別的語法來進行測試
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    // 因為只對邏輯做測試，不對樣式測試，所以需將以下靜態資源轉換成字符串即可，不需要解析
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    // 將 es6 等轉成 es5 語法
     '^.+\\.jsx?$': 'babel-jest'
   },
-  transformIgnorePatterns: [
-    '/node_modules/'
-  ],
+  // 不需要轉換的路徑檔案
+  transformIgnorePatterns: ['/node_modules/'],
+  // 因為 Jest 不知道 vue 配置的引入前綴 @ 意思，在此重新映射轉換成對應路徑
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
+  snapshotSerializers: ['jest-serializer-vue'],
+  // 需要執行的測試檔案, 以下配置會針對所有 tests/unit資料夾底下的所有 .spec.js .spec.jsx .spec.ts .spec.tsx 檔案來運行測試
+  // 或 __tests__/ 資料夾底下的所有 .js .jsx .ts .tsx 檔案
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  // window、jsDOM 位置
   testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
